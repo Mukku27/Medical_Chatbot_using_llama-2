97cf39200f912e1cbfbbf956f79a3a231b4e2057
VemulapalliMukesh27 <vemulapallimukesh@gmail.com>
2024-09-14 22:01:48 +0530

    setup file created

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='Medical Chatbot',
+    version='0.0.0',
+    author='Mukesh Vemulapalli',
+    author_email='vemulapallimukesh@gmail.com',
+    packages=find_packages(),
+    install_requires=[],
+)
